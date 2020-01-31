8f0752c07d4c3cdd9157cb21b02ce00301dd17f7
tzvaita <tzvaita@gmail.com>
2020-01-31 19:22:47 +0200

    fixed all rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -1,47 +1,42 @@
 #!/usr/bin/env ruby
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 count = 0
-input = 0
-GameFinished = false
+GAME_FINISHED = false
 
-puts "What is the name of the first player?"
-player1 =  gets.chomp!
+puts 'What is the name of the first player?'
+player1 = gets.chomp!
 
-puts "What is the name of the second player?"
-player2 =  gets.chomp!
+puts 'What is the name of the second player?'
+player2 = gets.chomp!
 
-def printBoard (i)
-  puts "  #{i[0]}  | #{i[1]} | #{i[2]}  "
-  puts "--------------"
-  puts "  #{i[3]}  | #{i[4]} | #{i[5]}  "
-  puts "--------------"
-  puts "  #{i[6]}  | #{i[7]} | #{i[8]}  "
+def print_board(param)
+  puts "  #{param[0]}  | #{param[1]} | #{param[2]}  "
+  puts '--------------'
+  puts "  #{param[3]}  | #{param[4]} | #{param[5]}  "
+  puts '--------------'
+  puts "  #{param[6]}  | #{param[7]} | #{param[8]}  "
 end
 
-printBoard(board)
+print_board(board)
 
-while GameFinished == false do
+while GAME_FINISHED == false
   count += 1
-  if count % 2 == 1
+  if count.odd?
     puts "what is #{player1}'s move?"
     # logic has to check if the position has already been chosen by a player and if so, ask for another move
     # Logic should also make sure the input is an integer between 1 to 9, if not, ask for a valid input
     input = gets.chomp!.to_i
     input -= 1
-    board[input] = "X"
-  else 
+    board[input] = 'X'
+  else
     puts "what is #{player2}'s move?"
     # logic has to check if the position has already been chosen by a player and if so, ask for another move
     # Logic should also make sure the input is an integer between 1 to 9, if not, ask for a valid input
     input = gets.chomp!.to_i
-    input -= 1 
-    board[input] = "O"
+    input -= 1
+    board[input] = 'O'
   end
-  printBoard(board)
-  #here logic should chech if someone has won
-  GameFinished = true if count == 10
+  print_board(board)
+  # here logic should chech if someone has won
+  GAME_FINISHED = true if count == 10
 end
-
-
-
-
