0f53bbc5746d33cceeeeb09c28f9caf1bf77283e
Fernando Del Valle <ferdelvalle@me.com>
2020-02-10 16:00:51 -0600

    Organizes the code in classes
    Organizes the interface and logic of the game in classes

--- bin/main.rb
+++ bin/main.rb
@@ -1,95 +1,77 @@
 #!/usr/bin/env ruby
 require_relative '../lib/logic'
-#Class interface
-#Board pringing method
-def print_board(param)
-  print "\n  #{param[0]}"
-  print " ¦ ".yellow
-  print "#{param[1]}"
-  print " ¦ ".yellow
-  print "#{param[2]} \n "
-  puts '-----------'.yellow
-  print "  #{param[3]}"
-  print " ¦ ".yellow
-  print "#{param[4]}"
-  print " ¦ ".yellow
-  print "#{param[5]} \n "
-  puts '-----------'.yellow
-  print "  #{param[6]}"
-  print " ¦ ".yellow
-  print "#{param[7]}"
-  print " ¦ ".yellow
-  print "#{param[8]}  \n "
-end
 
-#Input checking methods
-def error_entry(parameter)
-  while valid_check(parameter) == false
-    puts "number 1-9 only".red
-    parameter = gets.chomp!
+#Responsible for collecting information and displaying it
+class Interface
+  # Prints the board
+  def self.display(param)
+    @parameter = param
+    print "\n  #{@parameter[0]}"
+    print " ¦ ".yellow
+    print "#{@parameter[1]}"
+    print " ¦ ".yellow
+    print "#{@parameter[2]} \n "
+    puts '-----------'.yellow
+    print "  #{@parameter[3]}"
+    print " ¦ ".yellow
+    print "#{@parameter[4]}"
+    print " ¦ ".yellow
+    print "#{@parameter[5]} \n "
+    puts '-----------'.yellow
+    print "  #{@parameter[6]}"
+    print " ¦ ".yellow
+    print "#{@parameter[7]}"
+    print " ¦ ".yellow
+    print "#{@parameter[8]}  \n "
   end
-  $valid_input = parameter
-end
 
-def repeat_entry (parameter)
-  while repeat_check(parameter) == false
-    puts "that has already been done!".red
-    parameter = gets.chomp!
-    error_entry(parameter)    
+  # Asks for a player's move, check if it's valid and if it is, returns it as integer
+  def self.collect_move(move, board)
+    @move = move
+    @board = board
+    while MovesInput.valid_check(@move) == false || MovesInput.repeat_check(@board, @move) == false
+      puts "Invalid input or space already taken".red
+      @move = gets.chomp!
+    end
+    return @move.to_i
   end
-  $final_input = parameter
-end
 
-# victory checking methods
-def victory_check(param)
-  if param == 1
-    $count = 9
-    print "\n #{$player1}"
-    print " wins!".green
-  elsif param == 2
-    $count = 9
-    print "\n #{$player2}"
-    print" wins!".green
+  # Checks if there is a winner, if there is, it finishes the game and announces the winner.
+  def self.victory_check (winner, player1, player2)
+    @winner = winner
+    @player1 = player1
+    @player2 = player2
+    if @winner == 1
+      print "\n#{player1}"" Wins!".green
+      $count = 9
+    elsif @winner == 2
+      print "\n#{player2}"" Wins!".green
+      $count = 9
+    end
   end
 end
 
-#playing method
-def playing (player, mark)
-  puts "\n what is #{player}'s move?"
-  entry = gets.chomp!
-  error_entry(entry)
-  repeat_entry($valid_input)
-  $input = $final_input.to_i
-  $input -= 1
-  $board[$input] = mark
-end
+# Game
 
-#this is the game
-
-$board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-$count = 0
-puts "Welcome to Tic Tac Toe by Tenny and Fer"
-
+puts "Wellcome to Tic Tac Toe by Tenny and Fer".green
+game = GameBoard.new
 puts "\nWhat is the name of the first player?".blue
-$player1 = gets.chomp!.blue
-
+player1 =  Players.new(gets.chomp.blue , "X".blue)
 puts "\nWhat is the name of the second player?".pink
-$player2 = gets.chomp!.pink
-
-print_board($board)
-
-while $count != 9
-  $count += 1
+player2 =  Players.new(gets.chomp.pink , "O".pink)
+$count = 0
+Interface.display(game.board)
+while $count !=9
+  $count +=1
   if $count.odd?
-    playing($player1, 'X'.blue)
+    # Player 1 move
+    puts "\n What is #{player1.name}'s move?'"
+    MovesInput.making_move(game.board, Interface.collect_move(gets.chomp, game.board), player1.mark)
   else
-    playing($player2, 'O'.pink)
+    #player 2 move
+    puts "\n What is #{player2.name}'s move?'"
+    MovesInput.making_move(game.board, Interface.collect_move(gets.chomp, game.board), player2.mark)
   end
-  print_board($board)
-  vcheck($board)
-  victory_check($winner)
-  hcheck($board)
-  victory_check($winner)
-  dcheck($board)
-  victory_check($winner)
+  Interface.display(game.board)
+  Interface.victory_check(WinChecks.global(game.board), player1.name, player2.name)
 end
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,20 +1,4 @@
-# Here is where the game logic goes
-# Class - Input checking
-def valid_check(param)
-  until param.match(/[1-9]/) && param.length < 2
-    return false
-  end
-end
-
-def repeat_check(param)
-  index = param.to_i
-  index -=1
-  if $board[index].is_a? String
-    return false
-  end
-end
-
-#Class String
+# Adds colors to the String class
 class String
   def colorize(color_code)
     "\e[#{color_code}m#{self}\e[0m"
@@ -41,75 +25,143 @@ class String
   end
 end
 
-#Class linear check
+# Logic
 
-def vcheck(arr)
-  $winner = 0
-  column =[]
-  vind1 = 0
-  vind2 = 3
-  vind3 = 6
-  while vind1 != 3
-    column << arr[vind1]
-    column << arr[vind2]
-    column << arr[vind3]
-
-    if column.all? { |x| x == 'X'.blue}
-      $winner = 1
-    elsif column.all? { |o| o == 'O'.pink }
-      $winner = 2
-    end
-    column =[]
-    vind1 += 1
-    vind2 += 1
-    vind3 += 1
+# Creates a board object which is an array from 1 to 9
+class GameBoard
+  attr_accessor :board
+  def initialize
+    @board = Array (1..9)
   end
-  return $winner
 end
 
-def hcheck(arr)
-  $winner = 0
-  row =[]
-  hind1 = 0
-  hind2 = 1
-  hind3 = 2
-  while hind1 < 7
-    row << arr[hind1]
-    row << arr[hind2]
-    row << arr[hind3]
-
-    if row.all? { |x| x == 'X'.blue}
-      $winner = 1
-    elsif row.all? { |o| o == 'O'.pink }
-      $winner = 2
-    end
-      row =[]
-    hind1 += 3
-    hind2 += 3
-    hind3 += 3
+# Creates an object for each new player with name and mark (X or O)
+class Players
+  attr_reader :name
+  attr_reader :mark
+  def initialize(name, mark)
+    @name = name
+    @mark = mark
   end
-  return $winner
 end
 
-def dcheck(arr)
-  $winner = 0
-  diagonal =[]
-  dind1 = 0
-  dind2 = 4
-  dind3 = 8
-  while dind1 < 7
-    diagonal << arr[dind1]
-    diagonal << arr[dind2]
-    diagonal << arr[dind3]
+# Makes the moves
+class MovesInput
 
-    if diagonal.all? { |x| x == 'X'.blue}
-      $winner = 1
-    elsif diagonal.all? { |o| o == 'O'.pink }
-      $winner = 2
-    end
-    diagonal =[]
-    dind1 += 6
-    dind3 -= 6
+  # Implementes the chosen move
+  def self.making_move(board, move, mark)
+    @move = move
+    @move -=1
+    @mark = mark
+    board[@move] = @mark
   end
-  return $winner
+
+  # Checks if the move input is valid
+  def self.valid_check(input)
+    @input = input
+    until @input.match(/[1-9]/) && @input.length < 2
+      return false
+    end
+  end
+
+  # Checks if the move has already been chosen
+  def self.repeat_check(board, input)
+    @index = input.to_i
+    @index -=1
+    return false if board[@index].is_a? String
+  end
+end
+
+# Checks for winning conditions and provides an output accordingly
+class WinChecks
+
+  def self.global(board)
+    @board = board
+    @revision = []
+    @revision << WinChecks.vertical(@board)
+    @revision << WinChecks.horizontal(@board)
+    @revision << WinChecks.diagonal(@board)
+    if @revision.any? { |i| i == 1}
+      return 1
+      elsif @revision.any? { |i| i == 2}
+      return 2
+    else
+      return 0
+    end
+  end
+
+  def self.vertical(board)
+    @board = board
+    @winner = 0
+    @column =[]
+    @vind1 = 0
+    @vind2 = 3
+    @vind3 = 6
+    while @vind1 != 3
+      @column << @board[@vind1]
+      @column << @board[@vind2]
+      @column << @board[@vind3]
+
+      if @column.all? { |x| x == "X".blue}
+        @winner = 1
+      elsif @column.all? { |o| o == "O".pink }
+        @winner = 2
+      end
+      @column =[]
+      @vind1 += 1
+      @vind2 += 1
+      @vind3 += 1
+    end
+    return @winner
+  end
+
+  def self.horizontal(board)
+    @board = board
+    @winner = 0
+    @row =[]
+    @hind1 = 0
+    @hind2 = 1
+    @hind3 = 2
+    while @hind1 < 7
+      @row << @board[@hind1]
+      @row << @board[@hind2]
+      @row << @board[@hind3]
+
+      if @row.all? { |x| x == "X".blue}
+        @winner = 1
+      elsif @row.all? { |o| o == "O".pink }
+        @winner = 2
+      end
+        row =[]
+      @hind1 += 3
+      @hind2 += 3
+      @hind3 += 3
+    end
+    return @winner
+  end
+
+  def self.diagonal(board)
+    @board = board
+    @winner = 0
+    @diagonal =[]
+    @dind1 = 0
+    @dind2 = 4
+    @dind3 = 8
+    while @dind1 < 7
+      @diagonal << @board[@dind1]
+      @diagonal << @board[@dind2]
+      @diagonal << @board[@dind3]
+
+      if @diagonal.all? { |x| x == "X".blue}
+        @winner = 1
+      elsif @diagonal.all? { |o| o == "O".pink }
+        @winner = 2
+      end
+      @diagonal =[]
+      @dind1 += 6
+      @dind3 -= 6
+    end
+    return @winner
+  end
+
 end
